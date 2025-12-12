ae93bc380136d6cc212a903dd2f2544fd2e7135b
Thariq Aulia Akbar <thariq2712@gmail.com>
2025-12-13 00:36:57 +0700

    first commit

---
+++ src/test/java/com/dibimbing/api_assignment/ApiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibimbing.api_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
