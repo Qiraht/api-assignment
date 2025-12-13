b4f67fc3e5006f0c2e2ec0b04bb64022bb0fcc33
Thariq Aulia Akbar <thariq2712@gmail.com>
2025-12-13 17:59:41 +0700

    create user controller and add register user endpoint

---
+++ src/main/java/com/dibimbing/api_assignment/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.dibimbing.api_assignment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dibimbing.api_assignment.dtos.UserReqRegister;
+import com.dibimbing.api_assignment.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserReqRegister request) {
+        userService.registerUser(request);
+        
+        
+        return ResponseEntity.ok("Register Sucessfully");
+
+    }
+}
