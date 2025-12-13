3a68035f89e570fc9b8f7dece8ff59c3b3527151
Thariq Aulia Akbar <thariq2712@gmail.com>
2025-12-13 18:45:51 +0700

    add user service for login

--- src/main/java/com/dibimbing/api_assignment/services/UserService.java
+++ src/main/java/com/dibimbing/api_assignment/services/UserService.java
@@ -1,9 +1,13 @@
 package com.dibimbing.api_assignment.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.dibimbing.api_assignment.dtos.UserReqRegister;
+import com.dibimbing.api_assignment.dtos.UserReqLogin;
 import com.dibimbing.api_assignment.entity.User;
 import com.dibimbing.api_assignment.repository.UserRepository;
 
@@ -20,6 +24,23 @@ public class UserService {
         BeanUtils.copyProperties(request, user);
 
         userRepo.save(user);
+    }
+
+    public ResponseEntity<String> loginUser(UserReqLogin request) {
+         Optional<User> userOpt = userRepo.findByEmail(request.getEmail());
+
+        if (userOpt.isEmpty()) {
+            return ResponseEntity.status(404).body("Email not found / incorrect email");
+         }
+
+        User user = userOpt.get();
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            return ResponseEntity.status(400).body("Incorrect password");
+        }
+
+        return ResponseEntity.ok("Login successful");
+        
     }
     
 }
