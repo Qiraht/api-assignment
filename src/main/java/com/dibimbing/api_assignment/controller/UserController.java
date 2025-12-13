b9ab8a81ce278fe63b6332011bdbd947935ebb0a
Thariq Aulia Akbar <thariq2712@gmail.com>
2025-12-13 23:26:37 +0700

    feat/add getUserById controller

--- src/main/java/com/dibimbing/api_assignment/controller/UserController.java
+++ src/main/java/com/dibimbing/api_assignment/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.dibimbing.api_assignment.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dibimbing.api_assignment.dtos.UserReqLogin;
 import com.dibimbing.api_assignment.dtos.UserReqRegister;
+import com.dibimbing.api_assignment.dtos.UserResponse;
 import com.dibimbing.api_assignment.services.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,13 +27,17 @@ public class UserController {
     public ResponseEntity<String> registerUser(@RequestBody UserReqRegister request) {
         userService.registerUser(request);
         
-        
         return ResponseEntity.ok("Register Successfully");
 
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> loginUserController(@RequestBody UserReqLogin request) {
-       return userService.loginUser(request);
+        return userService.loginUser(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<UserResponse>> getUserByIdController(@PathVariable("id") Long id) {
+        return userService.getUserById(id);
     }
 }
