65257ab4b8d09da7ca19f5ba03ba8881ef17146b
Thariq Aulia Akbar <thariq2712@gmail.com>
2025-12-16 11:15:53 +0700

    feat/change method to get, follows requirements

--- src/main/java/com/dibimbing/api_assignment/controller/UserController.java
+++ src/main/java/com/dibimbing/api_assignment/controller/UserController.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dibimbing.api_assignment.dtos.UserReqLogin;
 import com.dibimbing.api_assignment.dtos.UserReqRegister;
 import com.dibimbing.api_assignment.dtos.UserResponse;
 import com.dibimbing.api_assignment.services.UserService;
@@ -23,15 +21,15 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserReqRegister request) {
+    @GetMapping("/register")
+    public ResponseEntity<String> registerUser(UserReqRegister request) {
         return userService.registerUser(request);
 
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> loginUserController(@RequestBody UserReqLogin request) {
-        return userService.loginUser(request);
+    @GetMapping("/login")
+    public ResponseEntity<String> loginUserController(@RequestParam String username, @RequestParam String password) {
+        return userService.loginUser(username, password);
     }
 
     @GetMapping("/{id}")
